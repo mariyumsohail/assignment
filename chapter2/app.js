94639afd37ee1aa85d2eb61ea092481c5c2e3831
mariyumsohail <155555622+mariyumsohail@users.noreply.github.com>
2024-05-04 08:02:01 +0500

    Add files via upload

---
+++ chapter2/app.js
@@ -0,0 +1,29 @@
+var userName ="mariyum";
+var myName = ("mariyum");
+var massage = "helloWorld";
+alert("helloworld")
+var studentName = "Mariyum Sohail";
+ var studentAge = 20;
+ var studentGender = "Female";
+ var studentAddress = "NAZIMABAD";
+var studentGrade = "A";
+
+
+alert("Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Gender: " + studentGender);
+alert("Address: " + studentAddress);
+alert("Grade: " + studentGrade);
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+var email = "mariyumsohail71@gmail.com";
+alert("Your email address is : " + email);
+
+var userName = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book" + userName )
+
+var book = "A smarter way to learn JavaScript";
+alert("I am currently reading the book: " + book);
+var sign = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+alert("sign");
---
+++ chapter3/app.js
@@ -0,0 +1,13 @@
+var age = 18; 
+alert("i am " + age + " years old");
+var number = ("14")
+alert("You have visited this site " + number + " times")
+var birthyear = ("2006")
+var birthYear = 2006;
+document.write("My birth year is " + birthYear);
+
+var visitorName = "mariyum";
+var productTitle = "T-shirt";
+var quantity = 5;
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
